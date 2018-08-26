a092b54a136ba2bb1709babd7fadd188c831b4dd
Octallium <octallium@anil.local>
2018-08-26 11:34:05 +0530

    added basic data types

---
+++ data_types/data_types.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "octy"
+	age := 8
+	weight := 30.45
+	adult := false
+
+	fmt.Printf("name: %T\t", name)
+	fmt.Printf("age: %T\t", age)
+	fmt.Printf("weight: %T\t", weight)
+	fmt.Printf("adult: %T\t", adult)
+
+}
