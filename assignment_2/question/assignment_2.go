99e872e0232b6551f923068bf530841470c4a56e
Octallium <octallium@anil.local>
2018-08-26 12:26:39 +0530

    added assignment no 2 question

---
+++ assignment_2/question/assignment_2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"math"
+)
+
+func main() {
+	// Part 1 -
+	// Print out the data types along with the values
+	var1 := math.Pi
+	var2 := "Hey Gopher!"
+	var3 := true
+	var4 := 49
+	// Print out should be in the following format - e.g var5 := "I am learning Go"
+	// Output ->
+	// Value: I am learning Go    Data Type: string
+
+	// // Part 2 -
+	// var5 := false
+	// // Guess the output and print out the data types
+	// if var5 == false {
+	// 	// print out after each variable declaration
+	// 	var1 = "Yo Gopher!"
+	// 	// insert print statement here
+	// 	var2 = 35 + 4673 / 127
+	// 	var3 = "James Bond"
+	// 	var4 = 3672 - 982 + 1293
+	// 	var5 = true
+	// }
+}
